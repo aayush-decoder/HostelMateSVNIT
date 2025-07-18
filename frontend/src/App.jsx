16dc7a69e81880fa414c339de731d2d9b5a61c1c
Aayush Prasad <aayushp336@gmail.com>
2025-07-18 12:14:34 +0530

    To production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ export default function App() {
     const name = localStorage.getItem("name");
 
     if (token && email) {
-      fetch("http://localhost:8000/me", {
+      fetch("https://hostelmate-nqe3.onrender.com/me", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -62,7 +62,7 @@ export default function App() {
       const firebaseUser = result.user;
       const idToken = await firebaseUser.getIdToken();
 
-      const res = await fetch("http://localhost:8000/login", {
+      const res = await fetch("https://hostelmate-nqe3.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ idToken }),
